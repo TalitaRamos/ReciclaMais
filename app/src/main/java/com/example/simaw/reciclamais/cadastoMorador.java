48b385f46f9e0b916595ea06fbf342fbf0ca9753
TalitaRamos <talitasantosrd@gmail.com>
2018-07-24 16:55:41 -0300

    tela de escolha de tipo de cadastro

---
+++ app/src/main/java/com/example/simaw/reciclamais/cadastoMorador.java
@@ -0,0 +1,13 @@
+package com.example.simaw.reciclamais;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastoMorador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadasto_morador);
+    }
+}
---
+++ app/src/main/java/com/example/simaw/reciclamais/cadastroCentro.java
@@ -0,0 +1,13 @@
+package com.example.simaw.reciclamais;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastroCentro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_centro);
+    }
+}
