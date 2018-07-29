93b0e8047bb846640e625a5b49a9f5511640ea58
TalitaRamos <talitasantosrd@gmail.com>
2018-07-28 22:28:13 -0300

    Cadastro específico do morador e do centro de coleta

--- app/src/main/java/com/example/simaw/reciclamais/cadastoMorador.java
+++ app/src/main/java/com/example/simaw/reciclamais/cadastoMorador.java
@@ -1,13 +1,42 @@
 package com.example.simaw.reciclamais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import Util.MaskUtil;
 
 public class cadastoMorador extends AppCompatActivity {
-
+    private EditText cpf;
+    private EditText telefoneMorador;
+    private EditText cepMorador;
+    private EditText nascimentoMorador;
+    private ImageView buttonCad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadasto_morador);
+
+        cpf = (EditText) findViewById(R.id.cpf);
+        telefoneMorador= (EditText)findViewById(R.id.telefoneMorador);
+        cepMorador=(EditText) findViewById(R.id.cepMorador);
+        nascimentoMorador=(EditText)findViewById(R.id.idadeMorador);
+        buttonCad=(ImageView)findViewById(R.id.buttonCad);
+
+        //ADICIONANDO AS MASCARAS
+        cpf.addTextChangedListener(MaskUtil.mask(cpf, MaskUtil.FORMAT_CPF));
+        telefoneMorador.addTextChangedListener(MaskUtil.mask(telefoneMorador, MaskUtil.FORMAT_FONE));
+        cepMorador.addTextChangedListener(MaskUtil.mask(cepMorador, MaskUtil.FORMAT_CEP));
+        nascimentoMorador.addTextChangedListener(MaskUtil.mask(nascimentoMorador,MaskUtil.FORMAT_DATE));
+
+        buttonCad.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent it = new Intent(cadastoMorador.this, MainActivityMorador.class);
+               // startActivity(it);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/simaw/reciclamais/cadastroCentro.java
+++ app/src/main/java/com/example/simaw/reciclamais/cadastroCentro.java
@@ -1,13 +1,40 @@
 package com.example.simaw.reciclamais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import Util.MaskUtil;
 
 public class cadastroCentro extends AppCompatActivity {
+    private EditText cnpj;
+    private EditText telefoneCentro;
+    private EditText cepCentro;
+    private ImageView buttonCadCentro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_centro);
+
+        cnpj = (EditText) findViewById(R.id.cnpj);
+        telefoneCentro= (EditText)findViewById(R.id.telefoneCentro);
+        cepCentro=(EditText) findViewById(R.id.cepCentro);
+        buttonCadCentro=(ImageView)findViewById(R.id.buttonCadCentro);
+
+        cnpj.addTextChangedListener(MaskUtil.mask(cnpj, MaskUtil.FORMAT_CNPJ));
+        telefoneCentro.addTextChangedListener(MaskUtil.mask(telefoneCentro, MaskUtil.FORMAT_FONE));
+        cepCentro.addTextChangedListener(MaskUtil.mask(cepCentro, MaskUtil.FORMAT_CEP));
+
+        /*buttonCadCentro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(cadastroCentro.this, MainActivityCentro.class);
+                startActivity(it);
+            }
+        });*/
+
     }
 }
